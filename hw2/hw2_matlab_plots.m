201113459ceb4f99e474ef2155e97c03edd77f53
Cameron Sullivan <arrowc11@gmail.com>
2018-09-12 23:59:49 -0400

    Quick HW2 additions

--- hw2/hw2_matlab_plots.m
+++ hw2/hw2_matlab_plots.m
@@ -2,13 +2,17 @@
 
 % Plot Vsig
 Array1 = csvread('hw2-num4c-vsig.csv');
-t = Array(:, 1);
-Vsig = Array(:, 2);
-plot(t, vsig);
+t = Array1(:, 1);
+Vsig = Array1(:, 2);
+plot(t, Vsig);
+title('Level Shifter Transient Analysis Voltage');
+xlabel('Time, s');
+ylabel('Voltage, V');
+grid on;
 hold on;
 
 % Plot Vin
 Array2 = csvread('hw2-num4c-vin.csv');
-t = Array(:, 1);
-Vin = Array(:, 2);
+t = Array2(:, 1);
+Vin = Array2(:, 2);
 plot(t, Vin);
