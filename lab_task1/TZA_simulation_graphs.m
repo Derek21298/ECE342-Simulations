ddf96e27387a49e773829d5622620bfd7b01d448
Cameron Sullivan <arrowc11@gmail.com>
2018-09-21 18:00:16 -0400

    Task 1: Graph for Derek

--- lab_task1/TZA_simulation_graphs.m
+++ lab_task1/TZA_simulation_graphs.m
@@ -1,23 +1,25 @@
 % ECE 342 - Cameron Sullivan - Lab Task 1 MATLAB code
 
+close all;
+
 Array1 = csvread('TZ_amplifier_Gain_dB.CSV');
-frequency1 = Array1(:, 1) / 10e2;
+frequency1 = Array1(:, 1);
 dB_gain = Array1(:, 4);
 
 Array2 = csvread('TZ_amplifier_Gain_Ohms.CSV');
-frequency2 = Array2(:, 1) / 10e2;
+frequency2 = Array2(:, 1);
 ohm_gain = Array2(:, 4) / 10e2;
 
 subplot(2,1,1);
-plot(frequency, dB_gain, 'linewidth', 1.5);
+semilogx(frequency1, dB_gain, 'linewidth', 1.5);
 title('Transimpedance Amplifier Simulated Frequency Response');
-xlabel('Frequency, KHz');
+xlabel('Frequency, Hz');
 ylabel('Gain, dB');
 grid on;
 
 subplot(2,1,2);
-plot(frequency, ohm_gain, 'linewidth', 1.5);
+semilogx(frequency2, ohm_gain, 'linewidth', 1.5);
 title('Transimpedance Amplifier Simulated Frequency Response');
-xlabel('Frequency, KHz');
+xlabel('Frequency, Hz');
 ylabel('Gain, kilo-ohms');
 grid on;
