3223bc724e07c996a4b2c44346ba2beded0246b7
Cameron Sullivan <arrowc11@gmail.com>
2018-09-30 16:44:44 -0400

    Task 1: design fix

---
+++ lab_task1/TZA_experimental_graphs.m
@@ -0,0 +1,18 @@
+% ECE 342 - Cameron Sullivan - Lab Task 1 Experimental Graphs
+
+close all;
+
+Array = csvread('TZA_experimental_f_response_FINAL.csv');
+frequency = Array(:,1);
+dB_gain = Array(:,2);
+
+semilogx(frequency, dB_gain, 'linewidth', 1.5);
+title('TZA Frequency Response');
+ylabel('Gain, dB');
+xlabel('Frequency, Hz');
+hold on;
+grid on;
+
+f3db = interp1(dB_gain,frequency,-3)
+
+line([f3db f3db], [-100 -3],'linestyle', '--');
