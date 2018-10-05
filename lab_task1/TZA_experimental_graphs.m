1ef1e03e959763124d6ffec09c54a3bdd34f41d3
Cameron Sullivan <arrowc11@gmail.com>
2018-10-04 23:18:20 -0400

    Task 1: new photos for report

--- lab_task1/TZA_experimental_graphs.m
+++ lab_task1/TZA_experimental_graphs.m
@@ -9,11 +9,13 @@ dB_gain = Array(:,2);
 
 semilogx(frequency, dB_gain, 'linewidth', 1.5);
 % title('TZA Frequency Response - R_{2} = 150k\Omega');
+set(gca,'fontsize',20); % set font size
 ylabel('Gain, dB');
 xlabel('Frequency, Hz');
 hold on;
 grid on;
+xlim([0 2e6]);
+ylim([-50, 10]);
 
 f3db = interp1(dB_gain,frequency,-3)
-
 line([f3db f3db], [-100 -3],'linestyle', '--');
--- lab_task1/TZA_simulation_graphs.m
+++ lab_task1/TZA_simulation_graphs.m
@@ -10,9 +10,10 @@ Array2 = csvread('TZ_amplifier_Gain_Ohms.CSV');
 frequency2 = Array2(:, 1);
 ohm_gain = Array2(:, 4) / 10e2;
 
-subplot(2,1,1);
+% subplot(2,1,1);
 semilogx(frequency1, dB_gain, 'linewidth', 1.5);
 %title('Transimpedance Amplifier Simulated Frequency Response');
+set(gca,'fontsize',20); % set font size
 xlabel('Frequency, Hz');
 ylabel('Gain, dB');
 hold on;
@@ -21,9 +22,9 @@ f3db = interp1(dB_gain,frequency1,102.2)
 line([f3db f3db], [90 102.2],'linestyle', '--');
 hold off;
 
-subplot(2,1,2);
-semilogx(frequency2, ohm_gain, 'linewidth', 1.5);
-%title('Transimpedance Amplifier Simulated Frequency Response');
-xlabel('Frequency, Hz');
-ylabel('Gain, kilo-ohms');
-grid on;
+% subplot(2,1,2);
+% semilogx(frequency2, ohm_gain, 'linewidth', 1.5);
+% %title('Transimpedance Amplifier Simulated Frequency Response');
+% xlabel('Frequency, Hz');
+% ylabel('Gain, kilo-ohms');
+% grid on;
