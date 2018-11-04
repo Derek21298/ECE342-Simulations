0b64c1081e9d6f90108a28989838181b2c9c779a
Cameron Sullivan <arrowc11@gmail.com>
2018-11-04 13:19:13 -0500

    Task 3: CMOS transient data

--- lab_task3/part1/part1_exper.m
+++ lab_task3/part1/part1_exper.m
@@ -1,10 +1,31 @@
 %% ECE 342 Lab Task 3 Part 1
+close all;
 
-input = csvread('nmos-exper-vtc-INPUT.csv');
-output = csvread('nmos-exper-vtc-OUTPUT.csv');
+input = csvread('nmos-exper-vtc-INPUT-test.csv');
+output = csvread('nmos-exper-vtc-OUTPUT-test.csv');
 
 inputVoltage = input(:,2);
 outputVoltage = output(:,2);
 
-plot(inputVoltage, outputVoltage);
+plot(inputVoltage, outputVoltage, 'Linewidth', 2);
 grid on;
+set(gca, 'fontsize', 16);
+title('Experimental NMOS Inverter VTC');
+xlabel('Vin, V');
+ylabel('Vout, V');
+
+
+%% Transient response
+
+output = csvread('nmos-exper-transient.csv');
+
+time = output(:,1);
+outputVoltage = output(:,2);
+
+plot(time * 10e2, outputVoltage, 'Linewidth', 2);
+xlim([-2.6 2.6]);
+grid on;
+set(gca, 'fontsize', 16);
+title('Experimental NMOS Inverter Transient Response');
+ylabel('Vout, V');
+xlabel('Time, ms');
--- lab_task3/part2/task3_part2_experimental.m
+++ lab_task3/part2/task3_part2_experimental.m
@@ -1,18 +1,38 @@
 %% ECE 342 Lab Task 3 Part 2
 close all;
 
-input = csvread('cmos-exper-transient-INPUT.csv');
-output = csvread('cmos-exper-transient-OUTPUT.csv');
+input = csvread('cmos-vtc-input-exper.csv');
+output = csvread('cmos-vtc-output-exper.csv');
 
 inputVoltage = input(:,2);
 outputVoltage = output(:,2);
 inputVSub = inputVoltage(544:545);
 outputVSub = outputVoltage(544:545);
 
-plot(inputVoltage, outputVoltage);
+plot(inputVoltage, outputVoltage, 'Linewidth', 2);
 grid on;
+set(gca, 'fontsize', 18);
+title('Experimental CMOS Inverter VTC');
+ylabel('V_{O1}, V');
+xlabel('V_{in}, V');
 hold on;
 
 halfwayPoint = interp1(outputVSub, inputVSub, 2.5)
 line([halfwayPoint halfwayPoint], [0 2.5],'linestyle', '--');
-text(2.5, 2.5, 'V_{in} = 2.44 V, V_{out} = 2.5 V');
+text(2.5, 2.5, 'V_{in} = 2.44 V, V_{out} = 2.5 V', 'fontsize', 18);
+
+%% Transient
+close all;
+
+output = csvread('cmos-transient-exper.csv');
+
+time = output(:,1);
+outputVoltage = output(:,2);
+
+plot(time, outputVoltage, 'Linewidth', 2);
+grid on;
+set(gca, 'fontsize', 18);
+title('Experimental CMOS Inverter Transient Response');
+ylabel('V_{O1}, V');
+xlabel('Time, s');
+hold on;
