f1517075def0b6068235fd810bcb8120383fa00f
Cameron Sullivan <arrowc11@gmail.com>
2018-11-19 00:52:11 -0500

    HW11: done

---
+++ hw11/hw11_4a.m
@@ -0,0 +1,44 @@
+%% Cameron Sullivan - ECE 342 - HW 11 #3 part A Plots
+close all;
+
+%% Circuit 1
+
+data = csvread('basic_2N3904_circuit1.CSV');
+temperature = data(:,1);
+I_C = data(:,3);
+
+figure(1);
+plot(temperature, I_C * 10e2, 'linewidth', 2);
+grid on;
+set(gca, 'fontsize', 20);
+title('Circuit 1: Collector Current vs Circuit Temperature');
+ylabel('I_{C}, mA');
+xlabel('Temperature, degrees C');
+
+%% Circuit 2
+
+data = csvread('basic_2N3904_circuit2.CSV');
+temperature = data(:,1);
+I_C = data(:,3);
+
+figure(2);
+plot(temperature, I_C * 10e2, 'linewidth', 2);
+grid on;
+set(gca, 'fontsize', 20);
+title('Circuit 2: Collector Current vs Circuit Temperature');
+ylabel('I_{C}, mA');
+xlabel('Temperature, degrees C');
+
+%% Circuit 3
+
+data = csvread('basic_2N3904_circuit3.CSV');
+temperature = data(:,1);
+I_C = data(:,3);
+
+figure(3);
+plot(temperature, I_C * 10e2, 'linewidth', 2);
+grid on;
+set(gca, 'fontsize', 20);
+title('Circuit 3: Collector Current vs Circuit Temperature');
+ylabel('I_{C}, mA');
+xlabel('Temperature, degrees C');
