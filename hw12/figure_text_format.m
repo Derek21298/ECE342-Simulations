6549554006d84033b024595930de74767ba845d2
Cameron Sullivan <arrowc11@gmail.com>
2018-11-27 20:24:06 -0500

    HW12 preliminary answers

--- hw12/figure_text_format.m
+++ hw12/figure_text_format.m
@@ -5,8 +5,8 @@ function output_txt = myfunction(obj,event_obj)
 % output_txt   Data cursor text string (string or cell array of strings).
 
 pos = get(event_obj,'Position');
-output_txt = {['X: ',num2str(pos(1),2)],...
-    ['Y: ',num2str(pos(2),2)]};
+output_txt = {['X: ',num2str(pos(1),4)],...
+    ['Y: ',num2str(pos(2),4)]};
 
 % If there is a Z-coordinate in the position, display it as well
 if length(pos) > 2
--- hw12/hw12.m
+++ hw12/hw12.m
@@ -1,6 +1,6 @@
 %% ECE 342 - Cameron Sullivan - HW12
 
-%% #3 Part A: NMOS amplifier gain vs input voltage
+%% #2 Part A: NMOS amplifier gain vs input voltage
 close all;
 
 input = csvread('nmos-exper-vtc-INPUT-test.csv');
@@ -10,11 +10,12 @@ inputVoltage = input(:,2);
 outputVoltage = output(:,2);
 
 gain = outputVoltage ./ inputVoltage;
-plot(inputVoltage, gain, 'linewidth', 2);
+plot(inputVoltage, gain, 'linewidth', 2.2);
 set(gca, 'fontsize', 20);
 title('NMOS Amplifier Gain vs Input Voltage');
 ylabel('A_{v}, V/V');
 xlabel('V_{GS}, V');
+xlim([0.01243, 4.5]);
 % figure(2);
 % plot(inputVoltage, outputVoltage, 'linewidth', 2);
 grid on;
@@ -27,7 +28,7 @@ vin = data(:,1);
 vout = data(:,3);
 
 figure(1);
-plot(vin, vout, 'linewidth', 2);
+plot(vin, vout, 'linewidth', 2.2);
 grid on;
 set(gca, 'fontsize', 20);
 title('BJT Amplifier VTC');
@@ -42,7 +43,7 @@ frequency = data(:,1);
 gain = data(:,4);
 
 figure(1);
-semilogx(frequency, abs(gain), 'linewidth', 2);
+semilogx(frequency, abs(gain), 'linewidth', 2.2);
 grid on;
 set(gca, 'fontsize', 20);
 title('BJT Amplifier Gain Response');
