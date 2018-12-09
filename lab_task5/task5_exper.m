a1400ea9da8e166f8d75038f2265e72ac38f14ff
Cameron Sullivan <arrowc11@gmail.com>
2018-12-09 12:44:10 -0500

    Task 5: experimental done

--- lab_task5/task5_exper.m
+++ lab_task5/task5_exper.m
@@ -13,7 +13,7 @@ figure(1);
 semilogx(frequency, voltage, 'linewidth', 2);
 grid on;
 set(gca, 'fontsize', 20);
-% title('Common Source Amplifier Experimental Frequency Response');
+% title('Common Source Amplifier Experimental Frequency Response (Unloaded)');
 ylabel('A_{VO} (Unloaded Gain), dB');
 xlabel('Frequency, Hz');
 % ylim([ ]);
@@ -21,6 +21,22 @@ xlabel('Frequency, Hz');
 hold on;
 
 %% CS Amplifier Experimental AV
+close all;
+
+data = csvread('cs_exper_AV.csv');
+frequency = data(:,1);
+voltage = data(:,2);
+
+figure(1);
+semilogx(frequency, voltage, 'linewidth', 2);
+grid on;
+set(gca, 'fontsize', 20);
+% title('Common Source Amplifier Experimental Frequency Response (Loaded)');
+ylabel('A_{V} (Loaded Gain), dB');
+xlabel('Frequency, Hz');
+% ylim([ ]);
+% xlim([ ]);
+hold on;
 
 %% CE Amplifier Experimental AVO
 close all;
@@ -33,7 +49,7 @@ figure(1);
 semilogx(frequency, voltage, 'linewidth', 2);
 grid on;
 set(gca, 'fontsize', 20);
-% title('Common Emitter Amplifier Experimental Frequency Response');
+title('Common Emitter Amplifier Experimental Frequency Response');
 ylabel('A_{VO} (Unloaded Gain), dB');
 xlabel('Frequency, Hz');
 % ylim([ ]);
@@ -41,3 +57,19 @@ xlabel('Frequency, Hz');
 hold on;
 
 %% CE Amplifier Experimental AV
+close all;
+
+data = csvread('ce_exper_AV.csv');
+frequency = data(:,1);
+voltage = data(:,2);
+
+figure(1);
+semilogx(frequency, voltage, 'linewidth', 2);
+grid on;
+set(gca, 'fontsize', 20);
+% title('Common Emitter Amplifier Experimental Frequency Response (Loaded)');
+ylabel('A_{V} (Loaded Gain), dB');
+xlabel('Frequency, Hz');
+% ylim([ ]);
+% xlim([ ]);
+hold on;
